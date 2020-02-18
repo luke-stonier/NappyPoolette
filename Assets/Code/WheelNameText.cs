289797b6b29d9601aa02621da396deb16d631439
luke stonier <lukestonier@hotmail.com>
2020-02-18 10:47:57 +0000

    Started work on wheel

---
+++ Assets/Code/WheelNameText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class WheelNameText : MonoBehaviour
+{
+    public string MySortingLayer;
+    public int MySortingOrderInLayer;
+
+    public void setSort()
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.sortingLayerName = MySortingLayer;
+        renderer.sortingOrder = MySortingOrderInLayer;
+    }
+}
+
+[CustomEditor(typeof(WheelNameText))]
+public class WheelNameEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        WheelNameText wnt = (WheelNameText)target;
+        if (GUILayout.Button("Sort"))
+        {
+            wnt.setSort();
+        }
+    }
+}
