4d3f537cd382d929640539c0376bc92f91fddc9e
luke stonier <lukestonier@hotmail.com>
2020-02-26 13:22:14 +0000

    Added banner AD

--- Assets/Code/AdManager.cs
+++ Assets/Code/AdManager.cs
@@ -1,44 +1,25 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-// using GoogleMobileAds.Api;
-
+using UnityEngine.Advertisements;
 public class AdManager : MonoBehaviour
 {
-    // private BannerView bannerView;
+    string gameId = "3479815";
+    private string placementId = "BottomBanner";
+    bool testMode = true;
 
-    // public void Start()
-    // {
-    //     #if UNITY_ANDROID
-    //         string appId = "ca-app-pub-4698588719115567~7943883443";
-    //     #elif UNITY_IPHONE
-    //         string appId = "ca-app-pub-4698588719115567~6906133946";
-    //     #else
-    //         string appId = "unexpected_platform";
-    //     #endif
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
 
-    //     print(appId);
-    //     // Initialize the Google Mobile Ads SDK.
-    //     MobileAds.Initialize(appId);
-
-    //     this.RequestBanner();
-    // }
-
-    //  private void RequestBanner()
-    //  {
-    //     #if UNITY_ANDROID
-    //         string adUnitId = "ca-app-pub-4698588719115567/8613983197";
-    //     #elif UNITY_IPHONE
-    //         string adUnitId = "ca-app-pub-4698588719115567/5593052273";
-    //     #else
-    //         string adUnitId = "unexpected_platform";
-    //     #endif
-
-    //      // Create a 320x50 banner at the top of the screen.
-    //      bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
-    //      AdRequest request = new AdRequest.Builder().Build();
-    //      // Load the banner with the request.
-    //      bannerView.LoadAd(request);
-    //      bannerView.Show();
-    // }
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(placementId);
+    }
 }
